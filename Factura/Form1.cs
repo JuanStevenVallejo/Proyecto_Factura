7b52b099373ef940a91fb7f1c9a18726d26b1ddc
JuanStevenVallejo <101488436+JuanStevenVallejo@users.noreply.github.com>
2022-03-13 11:27:50 -0500

    Add files via upload

---
+++ Factura/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbProducto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int cod;
+            string nom;
+            float precio;
+
+            cod = cmbProducto.SelectedIndex;
+            nom = cmbProducto.SelectedItem.ToString();
+            precio = cmbProducto.SelectedIndex;
+
+            switch (cod)
+            {
+                case 0: lblCodigo.Text = "0011";break;
+                case 1: lblCodigo.Text = "0022";break;
+                case 2: lblCodigo.Text = "0033";break;
+                case 3: lblCodigo.Text = "0044";break;
+                case 4: lblCodigo.Text = "0055";break;
+                case 5: lblCodigo.Text = "0066";break;
+                default: lblCodigo.Text = "0077";break;
+            }
+
+            switch (nom)
+            {
+                case "Polo": lblNombre.Text = "Polo";break;
+                case "Gorra": lblNombre.Text = "Gorra";break;
+                case "Camisa": lblNombre.Text = "Camisa"; break;
+                case "Capucha": lblNombre.Text = "Capucha"; break;
+                case "Jeam": lblNombre.Text = "Jeam"; break;
+                case "Gabardina": lblNombre.Text = "Gabardina"; break;
+                default: lblNombre.Text = "joger";break;
+            }
+
+            switch (precio)
+            {
+                case 0: lblPrecio.Text = "25";break;
+                case 1: lblPrecio.Text = "5";break;
+                case 2: lblPrecio.Text = "15"; break;
+                case 3: lblPrecio.Text = "30"; break;
+                case 4: lblPrecio.Text = "20"; break;
+                case 5: lblPrecio.Text = "15"; break;
+                default: lblPrecio.Text = "10";break;
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow file = new DataGridViewRow();
+            file.CreateCells(dgvLista);
+
+            file.Cells[0].Value = lblCodigo.Text;
+            file.Cells[1].Value = lblNombre.Text;
+            file.Cells[2].Value = lblPrecio.Text;
+            file.Cells[3].Value = txtCantidad.Text;
+            file.Cells[4].Value = (float.Parse(lblPrecio.Text) * float.Parse(txtCantidad.Text)).ToString();
+
+            //Agregar al data
+            dgvLista.Rows.Add(file);
+            //borrar los datos ingresados 
+            lblCodigo.Text = lblNombre.Text = lblPrecio.Text = txtCantidad.Text = "";
+            
+            obtenerTotal();
+
+
+        }
+        //metodo para que se me vaya sumando el precio de las compras 
+        public void obtenerTotal()
+        {
+            float costot = 0;
+            int contador = 0;
+
+            contador = dgvLista.RowCount;
+
+            for (int i = 0; i < contador; i++)
+           {
+                costot += float.Parse(dgvLista.Rows[i].Cells[4].Value.ToString());
+            }
+
+            lblTotatlPagar.Text = costot.ToString();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try {
+                DialogResult rppta = MessageBox.Show("¿Desea eliminar producto?",
+                    "Eliminacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (rppta == DialogResult.Yes)
+                {
+                    dgvLista.Rows.Remove(dgvLista.CurrentRow);
+                }
+            }
+            catch { }
+            //actualizar el valor total a pagar
+            obtenerTotal();
+        }
+
+        private void txtEfectivo_TextChanged(object sender, EventArgs e)
+        {
+            // dar el vuelto xd
+            try {
+                lblDevolucion.Text = (float.Parse(txtEfectivo.Text) - float.Parse(lblTotatlPagar.Text)).ToString();
+            }
+            catch { }
+
+            if (txtEfectivo.Text == "")
+            {
+                lblDevolucion.Text = "";
+            }
+
+        }
+
+        private void btnVender_Click(object sender, EventArgs e)
+        {
+            clsFactura.CreaTicket Ticket1 = new clsFactura.CreaTicket();
+
+            Ticket1.TextoCentro("Empresa de Ropa "); //imprime una linea de descripcion
+            Ticket1.TextoCentro("**********************************");
+
+            Ticket1.TextoIzquierda("");
+            Ticket1.TextoCentro("Factura de Venta"); //imprime una linea de descripcion
+            Ticket1.TextoIzquierda("No Fac: 0120102");
+            Ticket1.TextoIzquierda("Fecha:" + DateTime.Now.ToShortDateString() + " Hora:" + DateTime.Now.ToShortTimeString());
+            Ticket1.TextoIzquierda("Le Atendio: ");
+            Ticket1.TextoIzquierda("");
+            clsFactura.CreaTicket.LineasGuion();
+
+            clsFactura.CreaTicket.EncabezadoVenta();
+           clsFactura.CreaTicket.LineasGuion();
+            foreach (DataGridViewRow r in dgvLista.Rows)
+            {
+                // PROD                     //PrECIO                                    CANT                         TOTAL
+                Ticket1.AgregaArticulo(r.Cells[1].Value.ToString(), double.Parse(r.Cells[2].Value.ToString()), int.Parse(r.Cells[3].Value.ToString()), double.Parse(r.Cells[4].Value.ToString())); //imprime una linea de descripcion
+            }
+
+
+            clsFactura.CreaTicket.LineasGuion();
+            Ticket1.TextoIzquierda(" ");
+            Ticket1.AgregaTotales("Total", double.Parse(lblTotatlPagar.Text)); // imprime linea con total
+            Ticket1.TextoIzquierda(" ");
+            Ticket1.AgregaTotales("Efectivo Entregado:", double.Parse(txtEfectivo.Text));
+            Ticket1.AgregaTotales("Efectivo Devuelto:", double.Parse(lblDevolucion.Text));
+
+
+            // Ticket1.LineasTotales(); // imprime linea 
+
+            Ticket1.TextoIzquierda(" ");
+            Ticket1.TextoCentro("**********************************");
+            Ticket1.TextoCentro("*     Gracias por preferirnos    *");
+
+            Ticket1.TextoCentro("**********************************");
+            Ticket1.TextoIzquierda(" ");
+            string impresora = "Microsoft XPS Document Writer";
+            Ticket1.ImprimirTiket(impresora);
+
+            MessageBox.Show("Gracias por preferirnos");
+            
+            //this.Close();
+        }
+
+        public void Limpiar()
+        {
+            dgvLista.Columns.Clear();
+            txtEfectivo.Clear();
+        }
+
+            private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Desea Salir?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == System.Windows.Forms.DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
